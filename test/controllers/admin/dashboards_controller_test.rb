30a5aecfe89cb858a8a6593633705c7cba46e4a8
Sachi Urata (Saito) <126739734+sachiurata@users.noreply.github.com>
2025-09-09 08:31:39 +0000

    修正：テストを通すためのコード

--- test/controllers/admin/dashboards_controller_test.rb
+++ test/controllers/admin/dashboards_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class Admin::DashboardsControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
-    get admin_dashboards_top_url
+    get admin_root_path
     assert_response :success
   end
 
   test "should get heatmap" do
-    get admin_dashboards_heatmap_url
+    get admin_dashboards_heatmap_path
     assert_response :success
   end
 end
