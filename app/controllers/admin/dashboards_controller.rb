7a1cee17385722f5a6fbb5a71cf813dad3b0a2d9
Sachi Urata (Saito) <126739734+sachiurata@users.noreply.github.com>
2025-09-11 04:34:04 +0000

    交通需要マップのルート表示機能

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,5 +3,20 @@ class Admin::DashboardsController < Admin::BaseController
   end
 
   def heatmap
+    # @latitude = 38.78200655266919
+    # @longitude = 141.3628399833645
+
+    routes = RequestedRoute.all
+
+    # 各ルートの出発地の緯度・経度を抽出し、JSON形式に変換
+    # rgeoの機能により、latitudeを.y  longitudeを.x で値を取得する
+    @routes_data = routes.map do |route|
+      {
+        start_lat: route.start_point_location.y,
+        start_lng: route.start_point_location.x,
+        end_lat: route.end_point_location.y,
+        end_lng: route.end_point_location.x
+      }
+    end.to_json.html_safe
   end
 end
