679c013d3fece70eb181d033c39c59fc1a59fb3d
Sachi Urata (Saito) <126739734+sachiurata@users.noreply.github.com>
2025-10-08 08:27:27 +0000

    回答者情報登録機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "内容を登録しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :password, :password_confirmation, :postcode, :num_of_children)
   end
 end
