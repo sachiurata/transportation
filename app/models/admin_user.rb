8bd0038b3d4f46a0ca674d23f9ebc487d22fdcfe
Sachi Urata (Saito) <126739734+sachiurata@users.noreply.github.com>
2025-10-13 00:07:25 +0000

    テストエラー再々々々々々々々々々々々

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,6 +4,7 @@ class AdminUser < ApplicationRecord
 
   # emailの入力は必須、かつユニークであることを保証
   validates :email, presence: true, uniqueness: true
+  validates :role, presence: true
 
   # 管理者の権限設定（最低2つ以上の値を定義）
   enum role: { general: 0, superadmin: 1 }
