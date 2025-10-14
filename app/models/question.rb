47690712ddba8cf70af7777e785abcef12e923ab
Sachi Urata (Saito) <126739734+sachiurata@users.noreply.github.com>
2025-10-14 03:42:02 +0000

    テストエラー再々々々々々々々々々々々々々々々々々々々修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   has_many :question_options, dependent: :destroy
   has_many :answers, dependent: :destroy
 
-  enum question_type: { multiple_choice: 0, free_text: 1, multiple_choice_and_free_text: 2 }
+  enum :question_type, { multiple_choice: 0, free_text: 1, multiple_choice_and_free_text: 2 }
 
   validates :text, presence: true
   validates :question_type, presence: true
