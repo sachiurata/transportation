a1233066bbc6da8d1f241d7d28053e58b23e6ce3
Sachi Urata (Saito) <126739734+sachiurata@users.noreply.github.com>
2025-10-14 15:02:53 +0000

    テストエラー修正36

--- test/controllers/children_controller_test.rb
+++ test/controllers/children_controller_test.rb
@@ -8,21 +8,20 @@ class ChildrenControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_child_url
+    get new_user_child_url(@user)
     assert_response :success
   end
 
   test "should create child" do
     assert_difference("Child.count") do
-      post children_url, params: { child: {
-        user_id: @user.id,
+      post user_children_url(@user), params: { child: {
+        # user_id はURLから取得されるため、通常は不要です
         grade: 1,
         school_type: "0"
         # school_name や postcode など、他の属性も必要に応じて追加
       } }
     end
 
-    # 作成後のリダイレクト先が異なる場合は修正してください
     assert_redirected_to user_path(@user)
   end
 end
