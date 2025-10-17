5ec8578d8db63d994332ba74cfcb0e748685c620
Sachi Urata (Saito) <126739734+sachiurata@users.noreply.github.com>
2025-10-17 22:48:19 +0000

    エラー対応（テストファイル修正）

--- test/controllers/admin/questions_controller_test.rb
+++ test/controllers/admin/questions_controller_test.rb
@@ -1,8 +1,39 @@
 require "test_helper"
 
 class Admin::QuestionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get admin_questions_create_url
+  setup do
+    # テスト用の管理者ユーザーを取得し、ログインする
+    @admin_user = admin_users(:one)
+    post admin_session_url, params: { session: { user_name: @admin_user.user_name, password: "password" } }
+    assert_response :redirect
+
+    # このテストで質問を追加・表示する対象のアンケートを取得
+    @survey = surveys(:one)
+  end
+
+  test "should get index" do
+    get admin_survey_questions_url(@survey)
     assert_response :success
+  end
+
+  test "should get new" do
+    get new_admin_survey_question_url(@survey)
+    assert_response :success
+  end
+
+  test "should create question" do
+    # Question.countが1増えることを確認
+    assert_difference("Question.count") do
+      # createアクションにはPOSTリクエストを送る
+      post admin_survey_questions_url(@survey), params: {
+        question: {
+          text: "新しい質問",
+          question_type: "free_text" # または 1
+        }
+      }
+    end
+
+    # 作成後は、そのアンケートの質問一覧ページにリダイレクトされることを確認
+    assert_redirected_to admin_survey_questions_url(@survey)
   end
 end
--- test/controllers/admin/surveys_controller_test.rb
+++ test/controllers/admin/surveys_controller_test.rb
@@ -1,23 +1,38 @@
 require "test_helper"
 
 class Admin::SurveysControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # テスト用の管理者ユーザーを取得し、ログインする
+    @admin_user = admin_users(:one)
+    post admin_session_url, params: { session: { user_name: @admin_user.user_name, password: "password" } }
+    assert_response :redirect # ログイン後のリダイレクトを確認
+  end
+
   test "should get index" do
-    get admin_surveys_index_url
+    get admin_surveys_url
     assert_response :success
   end
 
   test "should get new" do
-    get admin_surveys_new_url
+    get new_admin_survey_url
     assert_response :success
   end
 
-  test "should get create" do
-    get admin_surveys_create_url
-    assert_response :success
+  test "should create survey" do
+    # Survey.countが1増えることを確認
+    assert_difference("Survey.count") do
+      # createアクションにはPOSTリクエストを送る
+      post admin_surveys_url, params: { survey: { survey_name: "新しいアンケート", admin_user_id: @admin_user.id } }
+    end
+
+    # 作成後は、作成されたアンケートの詳細ページにリダイレクトされることを確認
+    assert_redirected_to admin_survey_url(Survey.last)
   end
 
-  test "should get show" do
-    get admin_surveys_show_url
+  test "should show survey" do
+    # テスト用のアンケートデータを取得
+    @survey = surveys(:one)
+    get admin_survey_url(@survey) # 正しいヘルパー名と引数を指定
     assert_response :success
   end
 end
